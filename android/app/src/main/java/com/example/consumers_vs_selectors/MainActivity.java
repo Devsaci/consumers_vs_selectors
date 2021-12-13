ce3dc9cc8d08280e43ecf958cfd1191023a86462
saci zakaria <sacideveloppeur@gmail.com>
2021-12-13 10:39:39 +0100

    Consumers VS Selectors

---
+++ android/app/src/main/java/com/example/consumers_vs_selectors/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.consumers_vs_selectors;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import 'my_provider.dart';
+
+void main() => runApp(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (ctx) => MyProvider(),
+          )
+        ],
+        child: MyApp(),
+      ),
+    );
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Consumers VS Selectors"),
+      ),
+      body: DefaultTextStyle(
+        style: TextStyle(
+          fontSize: 30,
+          color: Colors.black,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              ' Provider Listen True : ${Provider.of<MyProvider>(context, listen: true).count}',
+            ),
+            Text(
+              ' Provider Listen False : ${Provider.of<MyProvider>(context, listen: true).count}',
+            ),
+            Text(
+              ' Provider Watch : ${context.watch<MyProvider>().count}',
+            ),
+            Consumer<MyProvider>(
+              builder: (ctx, value, child) => Text(
+                ' Consumer MyProvider : ${value.count}',
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => context.read<MyProvider>().increment(),
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
